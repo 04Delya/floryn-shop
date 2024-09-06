581291707cc0c1c0e14ad3510342e28a32ff8652
Delya04 <adellyaa04@gmail.com>
2024-09-07 01:41:02 +0700

    fix models.py in main app

--- main/models.py
+++ main/models.py
@@ -5,5 +5,5 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     desription = models.TextField()
-    category = models.CharField()
-    rating = models.DecimalField()
+    category = models.CharField(max_length=100)
+    rating = models.DecimalField(max_digits=5, decimal_places=2)
