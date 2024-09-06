b750bb46258532ad7b10c88cc1717728f29d39ed
Delya04 <adellyaa04@gmail.com>
2024-09-07 00:32:46 +0700

    configure Django project and run development server

--- floryn_shop/settings.py
+++ floryn_shop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$yp9drc6j@qr-$523)%v1jc$q)2_b##ff1g4h8r)q@r7v4sxw(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Application definition
