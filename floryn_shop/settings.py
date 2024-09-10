449cfed90da91f88791586b11dbe8c1b336f70fc
Delya04 <adellyaa04@gmail.com>
2024-09-10 20:16:20 +0700

    add URL deployment PWS

--- floryn_shop/settings.py
+++ floryn_shop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$yp9drc6j@qr-$523)%v1jc$q)2_b##ff1g4h8r)q@r7v4sxw(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "delya-ardiyanti-florynshop.pbp.cs.ui.ac.id"]
 
 
 # Application definition
