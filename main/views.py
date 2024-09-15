1e57d091592111c75c742caa2956b0a2ac0e1437
Delya04 <adellyaa04@gmail.com>
2024-09-15 23:47:20 +0700

    modified form and display for product

--- main/views.py
+++ main/views.py
@@ -1,11 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from main.forms import ProductForm
+from main.models import Product
 
 # Create your views here.
 def show_main(request):
+    products = Product.objects.all()
+
     context = {
         'npm' : '2306245586',
         'name': 'Delya Ardiyanti',
-        'class': 'PBP A'
+        'class': 'PBP A',
+        'products': products
     }
 
     return render(request, "main.html", context)
+
+def create_product(request):
+    form = ProductForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return redirect('main:show_main')
+
+    context = {'form': form}
+    return render(request, "create_product.html", context)
